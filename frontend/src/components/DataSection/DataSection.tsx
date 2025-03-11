f22ed2598c1271542406103b75108992d095b2e5
Tary Junior <111458237+TN-Junior@users.noreply.github.com>
2025-03-11 15:43:51 -0300

    feat(ui): atualizar instruções na DataSection

--- frontend/src/components/DataSection/DataSection.tsx
+++ frontend/src/components/DataSection/DataSection.tsx
@@ -2,7 +2,10 @@ const DataSection = () => {
   return (
     <div className="text-center mt-8">
       <h2 className="text-2xl font-semibold text-gray-700">DATA</h2>
-      <p className="text-gray-500 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p className="text-gray-500 text-sm">
+        Digite seu nome, sobrenome e participação, depois pressione enviar. 
+        Você pode atualizar e excluir qualquer um deles.
+      </p>
     </div>
   );
 };
